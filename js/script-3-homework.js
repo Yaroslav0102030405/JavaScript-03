17d6bc90e516890b362b95b9ebaf8cefb2f6d179
Yaroslav0102030405 <lukovetskyi@gmail.com>
2023-07-17 13:04:55 +0300

    update-33

---
+++ js/script-3-homework.js
@@ -0,0 +1,70 @@
+// Задача 1
+// const user = {
+//     name: 'Mango',
+//     age: 20,
+//     hobby: 'html',
+//     premium: true,
+//   };
+
+//   user.mood = "happy"
+//   user.hobby = 'skydiving'
+//   user.premium = "false"
+
+//   const keys = Object.keys(user)
+
+//   for (const key of keys) {
+//     console.log(`${key} : ${user[key]}`)
+//   }
+
+  // Задача 2
+//   const countProps = function(obj) {
+    
+//     const keys = Object.keys(obj)
+//     return keys.length
+//   };
+  
+
+//   console.log(countProps({})); // 0
+//   console.log(countProps({ name: 'Mango', age: 2 })); // 2
+//   console.log(countProps({ mail: 'poly@mail.com', isOnline: true, score: 500 })); // 3
+
+// Задача 3
+const findBestEmployee = function(employees) {
+    let maxValue = 0
+
+    
+    const keys = Object.entries(employees)
+    for (const [name, value] of employees) {
+        if (value > maxValue) {
+            maxValue = value
+    }
+    
+}
+
+  }
+  
+  console.log(
+    findBestEmployee({
+      ann: 29,
+      david: 35,
+      helen: 1,
+      lorence: 99,
+    }),
+  ); // lorence
+  
+  console.log(
+    findBestEmployee({
+      poly: 12,
+      mango: 17,
+      ajax: 4,
+    }),
+  ); // mango
+  
+  console.log(
+    findBestEmployee({
+      lux: 147,
+      david: 21,
+      kiwi: 19,
+      chelsy: 38,
+    }),
+  ); // lux
